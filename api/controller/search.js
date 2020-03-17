94b3922d22ab6ad6ce0e5a2c1be1d053c0a9f120
Preston Whitehall <pmwhitehall@gmail.com>
2020-03-16 17:42:03 -0700

    Refactors search api to use error first callbacks

--- api/controller/search.js
+++ api/controller/search.js
@@ -2,19 +2,22 @@ const NewsAPI = require('newsapi');
 const { newsApiKey } = require('../../config');
 const newsapi = new NewsAPI(newsApiKey);
 
-module.exports.queryTopHeadlines = (req, res, callback) => {
-  try {
-    const options = req.query;
-    newsapi.v2.topHeadlines(options)
-      .then(response => {
-        res.body = response;
-        callback(null, response);
-      })
-  } catch (err) {
-    callback(err);
-  }
+module.exports.queryTopHeadlines = (query, callback) => {
+  newsapi.v2.topHeadlines(query)
+    .then(response => {
+      callback(null, response);
+    })
+    .catch(err => {
+      callback(err, null);
+    });
 };
 
-module.exports.queryArticles = (req, callback) => {
-  // TODO
+module.exports.queryArticles = (query, callback) => {
+  newsapi.v2.everything(query)
+    .then(response => {
+      callback(null, response);
+    })
+    .catch(err => {
+      callback(err, null);
+    });
 };
--- api/search.js
+++ api/search.js
@@ -2,26 +2,31 @@ const { Router } = require('express');
 const search = require('./controller/search');
 const router = Router();
 
-const searchTopHeadlines = async (req, res) => {
-  try {
-    search.queryTopHeadlines(req, res, (err, data) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      res.status(200).json(data);
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(400);
-  }
+const searchTopHeadlines = (req, res) => {
+  search.queryTopHeadlines(req.query, (err, data) => {
+    if (err) {
+      console.log('ERROR:', err);
+      res.status(404).send('We encountered an error.');
+      return;
+    }
+    console.log('Data:', data);
+    res.status(200).json(data);
+  });
 };
 
-const searchArticles = async (req, res) => {
-  // TODO
+const searchArticles = (req, res) => {
+  search.queryArticles(req.query, (err, data) => {
+    if (err) {
+      console.log('ERROR:', err);
+      res.status(404).send('We encountered an error.');
+      return;
+    }
+    console.log('Article data:', data);
+    res.status(200).json(data);
+  });
 };
 
-router.get('/topHeadlines?', searchTopHeadlines);
+router.get('/top-headlines?', searchTopHeadlines);
 router.get('/articles?', searchArticles);
 
 module.exports = router;
