e2133c19e0a5eb556025b8d1738f2dc5e57d1d37
Preston Whitehall <pmwhitehall@gmail.com>
2020-04-16 01:35:58 -0700

    Fixes server to use heroku port or local port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(express.urlencoded());
 app.use(morgan('dev'))
