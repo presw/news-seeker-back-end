2c754c5e4ed34fefd677392dd0e274e23a94186b
Preston Whitehall <pmwhitehall@gmail.com>
2020-04-16 02:17:26 -0700

    Moving news api key to process.env for heroku deployment

--- api/controller/search.js
+++ api/controller/search.js
@@ -1,6 +1,6 @@
 const NewsAPI = require('newsapi');
-const { newsApiKey } = require('../../config');
-const newsapi = new NewsAPI(newsApiKey);
+const NEWS_API_KEY = process.env.NEWS_API_KEY;
+const newsapi = new NewsAPI(NEWS_API_KEY);
 
 module.exports.queryTopHeadlines = (query, callback) => {
   newsapi.v2.topHeadlines(query)
