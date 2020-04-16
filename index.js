efa08ed9fdda5d92e2d6fe0e579711129a0be210
Preston Whitehall <pmwhitehall@gmail.com>
2020-04-16 01:56:43 -0700

    Moves morgan to production dependencies to fix heroku crash

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.urlencoded());
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(cors());
 
 app.use('/api', require('./api'));
